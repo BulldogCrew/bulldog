64f955585288bf7a0a10a37b67d0c92e1943509a
dezzyboy <dezzyboy@tradebit.ca>
2019-08-14 16:06:30 -0400

    update copyright

--- src/zbull/accumulatorcheckpoints.json.h
+++ src/zbull/accumulatorcheckpoints.json.h
@@ -1,4 +1,5 @@
-// Copyright (c) 2018 The BULL developers
+// Copyright (c) 2018 The PIVX developers
+// Copyright (c) 2019 The BULL developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
